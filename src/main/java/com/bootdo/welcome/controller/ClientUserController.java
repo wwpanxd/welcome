4e6133e1075f9b2f97026e6e75ff3b94d64095f7
nhf <nhfc99@163.com>
2019-04-13 15:26:55 +0800

    更新用户操作

---
+++ src/main/java/com/bootdo/welcome/controller/ClientUserController.java
@@ -0,0 +1,73 @@
+package com.bootdo.welcome.controller;
+
+import com.bootdo.common.annotation.Log;
+import com.bootdo.common.controller.BaseController;
+import com.bootdo.common.utils.R;
+import com.bootdo.welcome.domain.UserDO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/wel/user")
+public class ClientUserController extends BaseController {
+	UserDO createUserDoModel() {
+		UserDO userDO = new UserDO();
+		userDO.setUserId(1L);
+		userDO.setUsername("登录用户名");
+		userDO.setName("真实姓名");
+		userDO.setDeptId(1L);
+		userDO.setDeptName("部门");
+		userDO.setEmail("邮箱");
+		userDO.setPost("用户职务");
+		userDO.setPhone("电话");
+		userDO.setStatus(1);
+		userDO.setUserIdCreate(new Date());
+		List<Long> list = new ArrayList<Long>();
+		list.add(1L);
+		list.add(2L);
+		userDO.setRoleIds(list);
+		userDO.setSex(1L);
+		userDO.setPicUrl("www.baidu.com");
+		userDO.setPicId(100L);
+		return userDO;
+	}
+
+	@Log("获取用户信息")
+	@ResponseBody
+	@PostMapping("/getUser")
+	public R getUserInfo(@RequestParam("userId") Long userId) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("data", createUserDoModel());
+		return R.ok(map);
+	}
+
+	@Log("新增用户")
+	@ResponseBody
+	@PostMapping("/addUser")
+	public R addUser(@RequestBody UserDO userDO) {
+		return R.ok();
+	}
+
+	@Log("更新用户")
+	@ResponseBody
+	@PostMapping("/updateUser")
+	public R updateUser(@RequestBody UserDO userDO) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("data", createUserDoModel());
+		return R.ok(map);
+	}
+	
+	@Log("删除用户")
+	@ResponseBody
+	@PostMapping("/deleteUser")
+	public R deleteUser(@RequestParam("userId") Long userId) {
+		return R.ok();
+	}
+}
---
+++ src/main/java/com/bootdo/welcome/domain/UserDO.java
@@ -0,0 +1,223 @@
+package com.bootdo.welcome.domain;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@ApiModel(value = "UserDO", description = "用户")
+public class UserDO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty(value = "用户ID")
+	private Long userId;
+
+	// 用户名
+	@ApiModelProperty(value = "登录名")
+	private String username;
+	// 用户真实姓名
+	@ApiModelProperty(value = "用户真实姓名")
+	private String name;
+	// 密码
+	private String password;
+	// 部门
+	private Long deptId;
+	private String deptName;
+	// 邮箱
+	private String email;
+	// 职务
+	@ApiModelProperty(value = "用户职务")
+	private String post;
+	// 电话
+	@ApiModelProperty(value = "电话")
+	private String phone;
+	@ApiModelProperty(value = "状态 0:禁用，1:正常")
+	private Integer status;
+	// 创建用户id
+	private Date userIdCreate;
+	// 创建时间
+	private Date userIdModified;
+
+	@ApiModelProperty(value = "归属角色列表")
+	private List<Long> roleIds;
+
+	@ApiModelProperty(value = "权限值list")
+	List<String> perms;
+
+	@ApiModelProperty(value = "性别")
+	private Long sex;
+//    //出身日期
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+//    private Date birth;
+
+	@ApiModelProperty(value = "头像url")
+	private String picUrl;
+
+	// 图片ID
+	private Long picId;
+
+//    //现居住地
+//    private String liveAddress;
+//    //爱好
+//    private String hobby;
+//    //省份
+//    private String province;
+//    //所在城市
+//    private String city;
+//    //所在地区
+//    private String district;
+
+	public UserDO() {
+
+	}
+
+	public UserDO(Long userId, String username, String name, Long deptId, String post, String phone,
+			List<String> perms) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.name = name;
+		this.deptId = deptId;
+		this.post = post;
+		this.phone = phone;
+		this.perms = perms;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(Long deptId) {
+		this.deptId = deptId;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public List<Long> getRoleIds() {
+		return roleIds;
+	}
+
+	public void setRoleIds(List<Long> roleIds) {
+		this.roleIds = roleIds;
+	}
+
+	public Long getSex() {
+		return sex;
+	}
+
+	public void setSex(Long sex) {
+		this.sex = sex;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getUserIdCreate() {
+		return userIdCreate;
+	}
+
+	public void setUserIdCreate(Date userIdCreate) {
+		this.userIdCreate = userIdCreate;
+	}
+
+	public Date getUserIdModified() {
+		return userIdModified;
+	}
+
+	public void setUserIdModified(Date userIdModified) {
+		this.userIdModified = userIdModified;
+	}
+
+	public List<String> getPerms() {
+		return perms;
+	}
+
+	public void setPerms(List<String> perms) {
+		this.perms = perms;
+	}
+
+	public String getPicUrl() {
+		return picUrl;
+	}
+
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+
+	public Long getPicId() {
+		return picId;
+	}
+
+	public void setPicId(Long picId) {
+		this.picId = picId;
+	}
+
+}
